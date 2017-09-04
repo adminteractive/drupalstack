088acd8e6e6cdbf1b2e48739be6869dddaa65d14
Jürgen Viljaste <j.viljaste@gmail.com>
2017-09-04 15:21:45 +0300

    Update

---
+++ bin/enter-service-httpd.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -e
+
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# Enter httpd service.                                                         #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+SCRIPT_PATH="$(dirname ${0})"
+SCRIPT_PATH="$(cd ${SCRIPT_PATH} && pwd)"
+
+PROJECT_ROOT="$(cd ${SCRIPT_PATH}/.. && pwd)"
+
+hash docker 2> /dev/null
+
+if [ "${?}" -ne 0 ]; then
+  echo "docker command not found."
+
+  exit 1
+fi
+
+httpd_container_exists() {
+  local PROJECT_ROOT="${1}"
+
+  echo "$(cd ${PROJECT_ROOT} && docker-compose -f docker-compose.yml ps httpd 2> /dev/null | grep _httpd_ | awk '{ print $1 }')"
+}
+
+httpd_container_running() {
+  local CONTAINER="${1}"
+
+  echo "$(docker exec ${CONTAINER} date 2> /dev/null)"
+}
+
+HTTPD_CONTAINER="$(httpd_container_exists ${PROJECT_ROOT})"
+
+if [ -z "${HTTPD_CONTAINER}" ]; then
+  read -p "httpd service could not be found. Would you like to start the containers? [Y/n]: " ANSWER
+
+  if [ "${ANSWER}" == "n" ]; then
+    exit
+  fi
+
+  cd "${PROJECT_ROOT}"
+
+  docker-compose -f docker-compose.yml up -d
+
+  HTTPD_CONTAINER="$(httpd_container_exists ${PROJECT_ROOT})"
+
+  echo "Waiting for httpd service to come up..."
+
+  sleep 30
+elif [ -z "$(httpd_container_running ${HTTPD_CONTAINER})" ]; then
+  read -p "httpd service is not running. Would you like to start the containers? [Y/n]: " ANSWER
+
+  if [ "${ANSWER}" == "n" ]; then
+    exit
+  fi
+
+  cd "${PROJECT_ROOT}"
+
+  docker-compose -f docker-compose.yml up -d
+
+  echo "Waiting for httpd service to come up..."
+
+  sleep 30
+fi
+
+docker exec -it "${HTTPD_CONTAINER}" bash
--- bin/enter-service-php.sh
+++ bin/enter-service-php.sh
@@ -4,13 +4,9 @@ set -e
 
 # ---------------------------------------------------------------------------- #
 #                                                                              #
-# Install Drupal 8 files.                                                      #
-#                                                                              #
-# Readme more: https://github.com/drupal-composer/drupal-project               #
+# Enter PHP service.                                                           #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
-
-WORKING_DIR="$(pwd)"
 
 SCRIPT_PATH="$(dirname ${0})"
 SCRIPT_PATH="$(cd ${SCRIPT_PATH} && pwd)"
@@ -21,14 +17,6 @@ hash docker 2> /dev/null
 
 if [ "${?}" -ne 0 ]; then
   echo "docker command not found."
-
-  exit 1
-fi
-
-hash docker-compose 2> /dev/null
-
-if [ "${?}" -ne 0 ]; then
-  echo "docker-compose command not found."
 
   exit 1
 fi
@@ -79,8 +67,4 @@ elif [ -z "$(php_container_running ${PHP_CONTAINER})" ]; then
   sleep 30
 fi
 
-docker exec -it "${PHP_CONTAINER}" bash -c "rm -rf /usr/local/apache2/htdocs/web"
-docker exec -it "${PHP_CONTAINER}" bash -c "composer create-project drupal-composer/drupal-project:8.x-dev /usr/local/apache2/htdocs --stability dev --no-interaction"
-docker exec -it "${PHP_CONTAINER}" bash -c "cd /usr/local/apache2/htdocs/web && echo y | ../vendor/drush/drush/drush site-install standard --db-url=mysql://root:root@db/drupal --account-mail=admin@example.com --account-name=admin --account-pass=admin --site-mail=admin@example.com --site-name=Drupalstack"
-
-cd "${WORKING_DIR}"
+docker exec -it "${PHP_CONTAINER}" bash

5b1b52d50cdd0fad96856374c376100b727082aa
Jürgen Viljaste <j.viljaste@gmail.com>
2017-09-04 14:57:38 +0300

    Update

---
+++ bin/show-logs.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# Show project services logs.                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+WORKING_DIR="$(pwd)"
+
+SCRIPT_PATH="$(dirname ${0})"
+SCRIPT_PATH="$(cd ${SCRIPT_PATH} && pwd)"
+
+PROJECT_ROOT="$(cd ${SCRIPT_PATH}/.. && pwd)"
+
+hash docker-compose 2> /dev/null
+
+if [ "${?}" -ne 0 ]; then
+  echo "docker-compose command not found."
+
+  exit 1
+fi
+
+cd "${PROJECT_ROOT}"
+
+docker-compose -f docker-compose.yml logs
+
+cd "${WORKING_DIR}"
